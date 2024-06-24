d144225d7e4b24d2a5116d17a7081ef9be31eec4
jonghunbaek <qor6028@naver.com>
2024-06-24 17:59:18 +0900

    feat : 이메일로 회원 조회 기능 추가 및 비밀번호 일치 여부 검증 메서드 추가

--- src/main/java/com/example/springevent/domain/Member.java
+++ src/main/java/com/example/springevent/domain/Member.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor
 @Entity
 public class Member {
@@ -14,8 +16,16 @@ public class Member {
     private Long id;
 
     private String email;
+    private String password;
 
-    public Member(String email) {
+    public Member(String email, String password) {
         this.email = email;
+        this.password = password;
+    }
+
+    public void validatePassword(String inputPassword) {
+        if (!this.password.equals(inputPassword)) {
+            throw new IllegalArgumentException("비밀번호 불일치");
+        }
     }
 }
--- src/main/java/com/example/springevent/repository/MemberRepository.java
+++ src/main/java/com/example/springevent/repository/MemberRepository.java
@@ -3,5 +3,10 @@ package com.example.springevent.repository;
 import com.example.springevent.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
 }
