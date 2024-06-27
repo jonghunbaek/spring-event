398b6ffbcfa25f82019d202fea44b0f0555173a6
jonghunbaek <qor6028@naver.com>
2024-06-27 18:38:04 +0900

    feat : TicketAuthFilter 추가, 변경
    - access token 디코딩하여 사용자 id 획득 메서드 추가
    - cache에서 값 가져올 때 NPE 발생하는 경우 고려해서 로직 수정 중

--- src/main/java/com/example/springevent/common/filter/TicketAuthFilter.java
+++ src/main/java/com/example/springevent/common/filter/TicketAuthFilter.java
@@ -1,6 +1,8 @@
 package com.example.springevent.common.filter;
 
 import com.example.springevent.domain.TicketCache;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,7 +15,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Map;
 import java.util.Optional;
+
+import static org.springframework.http.HttpHeaders.*;
 
 @RequiredArgsConstructor
 @Component
@@ -23,19 +30,50 @@ public class TicketAuthFilter extends OncePerRequestFilter {
     private final CacheManager cacheManager;
 
     @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String requestURI = request.getRequestURI();
+        System.out.println(requestURI);
+        return requestURI.contains("sign-in") || requestURI.contains("sign-up");
+    }
+
+    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         Cache cache = cacheManager.getCache("ticket");
 
-        // jwt에서 memberId 꺼내는 로직 추가
-        // memberId로 캐시 조회 - 현재는 임시로
-        Optional<TicketCache> ticketCacheOpt = Optional.ofNullable(cache.get(1L, TicketCache.class));
+        // access token 디코딩해서 사용자 id 추출
+        long memberId = getMemberId(request);
 
-        if (ticketCacheOpt.isEmpty()) {
-            eventPublisher.publishEvent(1L);
+        // memberId로 저장된 캐시 조회
+        TicketCache ticketCache;
+        try {
+            ticketCache = cache.get(memberId, TicketCache.class);
+        } catch (NullPointerException e) {
+            eventPublisher.publishEvent(memberId);
+            ticketCache = cache.get(memberId, TicketCache.class);
         }
 
-        ticketCacheOpt.get().validateRemainingTimes();
+
+        ticketCache.validateRemainingTimes();
 
         filterChain.doFilter(request, response);
+    }
+
+    private long getMemberId(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION);
+        String accessToken = bearerToken.substring(7);
+
+        return Long.parseLong(decodeJwtPayload(accessToken));
+    }
+
+    private String decodeJwtPayload(String accessToken) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            return objectMapper.readValue(new String(Base64.getDecoder().decode(accessToken.split("\\.")[1]), StandardCharsets.UTF_8), Map.class)
+                .get("sub")
+                .toString();
+        } catch (JsonProcessingException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
