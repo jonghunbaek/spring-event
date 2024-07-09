6f830aeaa7bd3e7fd2edbd5e1339d67f5aa63063
jonghunbaek <qor6028@naver.com>
2024-07-09 22:53:20 +0900

    refactor : 불필요 클래스 제거

--- src/main/java/com/example/springevent/repository/TicketCacheRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springevent.repository;
-
-import com.example.springevent.domain.TicketCache;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TicketCacheRepository extends CrudRepository<TicketCache, Long> {
-}
