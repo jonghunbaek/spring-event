9a2f2e02413e5a044f17170d74105edfdead5467
jonghunbaek <qor6028@naver.com>
2024-07-16 19:29:08 +0900

    feat : 이용권, 사용자 테스트 데이터 수정 및 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,10 @@
 INSERT INTO member (email, password)
 values
     ('abc@gmail.com', 'zxcv1234'),
+    ('eventException@gmail.com', 'zxcv1234'),
     ('noticket@gmail.com', 'zxcv1234');
 
 INSERT INTO consumable_ticket(title, remaining_times, member_id)
-values ('소모성 이용권', 10, 1);
+values
+    ('소모성 이용권', 10, 1),
+    ('소모성 이용권', 10, 2);
