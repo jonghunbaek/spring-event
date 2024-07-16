3a26bafc46762442d3eeee2ec8164d7f0af44804
jonghunbaek <qor6028@naver.com>
2024-07-16 19:26:22 +0900

    feat : 테스트용 회원 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,7 @@
 INSERT INTO member (email, password)
-values ('abc@gmail.com', 'zxcv1234');
+values
+    ('abc@gmail.com', 'zxcv1234'),
+    ('noticket@gmail.com', 'zxcv1234');
 
 INSERT INTO consumable_ticket(title, remaining_times, member_id)
 values ('소모성 이용권', 10, 1);
