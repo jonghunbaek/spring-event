932620c6c39c192753b3596b36cc05c8c6bcb04f
jonghunbaek <qor6028@naver.com>
2024-07-19 21:04:15 +0900

    feat : 이벤트 리스너 예외 발생 시 재시도 로직을 위한 retry 의존성 추가 및 설정, 로직 추가

--- src/main/java/com/example/springevent/common/event/TicketDeductEventListener.java
+++ src/main/java/com/example/springevent/common/event/TicketDeductEventListener.java
@@ -5,6 +5,8 @@ import com.example.springevent.domain.ConsumableTicket;
 import com.example.springevent.repository.ConsumableTicketRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
@@ -17,6 +19,7 @@ public class TicketDeductEventListener {
     private final TicketCacheManager ticketCacheManager;
     private final ConsumableTicketRepository ticketRepository;
 
+    @Retryable(retryFor = IllegalArgumentException.class, maxAttempts = 5, backoff = @Backoff(delay = 3000, maxDelay = 5000))
     @Transactional(propagation = Propagation.REQUIRES_NEW) // REQUIRES_NEW 아니면 예외 발생
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT) // 이벤트가 발행된 곳의 트랜잭션 커밋 후에 실행
     public void deductTicketCount(Long memberId) {
